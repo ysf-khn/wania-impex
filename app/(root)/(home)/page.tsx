651a6ccb8b7d5343c7ea612df9dbaf2522f60378
Yusuf <yusufmohd72@gmail.com>
2025-05-16 18:02:58 +0530

    OG Image

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -5,6 +5,36 @@ import SacredLogistics from "@/app/Components/SacredLogistics/SacredLogistics";
 import WhyChooseUs from "@/app/Components/WhyChooseUs/WhyChooseUs";
 import React from "react";
 
+export const metadata = {
+  title: "Wania Impex",
+  description:
+    "Largest Manufacturer & Exporter of Incense Burners & New Age Ritual Tools",
+  openGraph: {
+    title: "Wania Impex",
+    description:
+      "Largest Manufacturer & Exporter of Incense Burners & New Age Ritual Tools",
+    url: "https://www.waniaimpex.com",
+    siteName: "Wania Impex",
+    images: [
+      {
+        url: "https://www.waniaimpex.com/opengraph-image.png", // 👈 must be full URL
+        width: 1200,
+        height: 630,
+        alt: "Wania Impex - Great Products",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Wania Impex",
+    description:
+      "Largest Manufacturer & Exporter of Incense Burners & New Age Ritual Tools",
+    images: ["https://www.waniaimpex.com/opengraph-image.png"],
+  },
+};
+
 const page = () => {
   return (
     <div className="bg-gray-100 text-black">
